cf27f10350d0914c30ccc3c3e25d84c8ec835ecf
Bhuvan Chadha <54380864+bchadha1@users.noreply.github.com>
2020-11-08 18:49:12 -0500

    4Chan Data Collection

--- 4chan.py
+++ 4chan.py
@@ -1,22 +1,22 @@
+# importing packages
 import re
 from bs4 import BeautifulSoup
 from datetime import datetime as dt
-from datetime import date
 import urllib
 import requests
 from mongoengine import *
 
-uri = "mongodb://127.0.0.1:27017/?compressors=zlib"
+# connecting to mongodb with the help of mongoengine library
+uri = "mongodb://127.0.0.1:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false"
 connect(
-    db='admin',
-    host=uri,
+    db='trial',  # this the name of our database for storing the 4chan data
+    # The db is already present in the mongodb community server edition(mongodb compass),if not it will create one
+    host=uri
 )
 
 
+# now entering into collection class of the db which is threads class
 class Threads(DynamicDocument):
-
-    def commcount(self):
-        pass
 
     # time stamp
     def t_func(self, DateTimeField):
@@ -25,12 +25,16 @@ class Threads(DynamicDocument):
 
     time_stamp = DateTimeField(default=dt.utcnow)
 
-    # post number of 4chan
+    # post number in 4chan data
     no = StringField(min_length=2, required=False)
     # creators comment for each particular thread
     com = StringField(min_length=1, required=False)
     # Title of thread creator
     semantic_url = StringField(min_length=None, required=False, primary_key=True)
+
+    # count no of comments
+    def commcount(self):
+        pass
 
     meta = {
         'indexes': [{
@@ -42,52 +46,63 @@ class Threads(DynamicDocument):
     }
 
 
-class BaseMetaAbstract(object):
+# utility class for parsing the 4chan page and doing filtering 
+class utillabstract(object):
 
-    def _substitute_text_pattern(self, pat, replacement, text):
-        text = re.sub(pat, replacement, text)
-        return str(text)
+    # method(function) for substituting text with the help of regular expressions methods(re.sub)
+    def text_pattern_replacement(self, pattern, to_replace, text):
+        # takes pattern, and what to replace and text(which is to be replaced) as arguments to sub method
+        text = re.sub(pattern, to_replace, text)
+        substitued_text = str(text)
+        return substitued_text
 
-    def _parse_html_and_strip_special_chars(self, df):
-        soup = BeautifulSoup(str(df), 'lxml')
-        df = self._substitute_text_pattern(r'>|>>|>>>|>>>>|\d{9}', '', soup.get_text())
-        return df
+    # parsing the html content and stripping the special characters in it if exists
+    # calling the re expression replace function 
+    def parse_remove(self, cd):
+        # taking the content of html 4chan page into beautifulsoup object using lxml
+        soup_object = BeautifulSoup(str(cd), 'lxml')
+        cd = self.text_pattern_replacement(r'>|>>|>>>|>>>>|>>>>>|>>>>>>|>>>>>>>|\d[0-9]', '', soup_object.get_text())
+        return cd
 
 
-class GetChan(BaseMetaAbstract):
+# calling the 4chandata
+
+class getting_chan_data(utillabstract):
     _page = {}
-    _page_num = 0
+    page_number = None
 
-    def _catalog_page_generator(self, x):
-        for page_num in x[self._page_num]['threads']:
+    def catalog_page_generator(self, x):
+        for page_num in x[self.page_number]['threads']:
             yield page_num
 
-    def _catalog_generator(self, x):
+    def catalog_generator(self, x):
         for idx, page in enumerate(x):
             for thread in x[idx]['threads']:
                 yield thread
 
-    def get_catalog_page(self, board='pol', page_num=9):
+    # getting the json format of catalog page of pol data from 4chan
+    def get_catalog_page(self, board='pol', page_num=7):
         r = requests.get(f'https://a.4cdn.org/{board}/catalog.json').json()
-        self._page_num = page_num
+        self.page_number = page_num
 
-        for threads in self._catalog_page_generator(r):
+        for threads in self.catalog_page_generator(r):
             self._page['no'] = str(threads.get('no'))
             self._page['com'] = threads.get('com')
-            # self._page['user_id'] = threads.get('user_id')
             self._page['semantic_url'] = threads.get('semantic_url')
 
             thread_pages = Threads(
                 postnum=self._page['no'],
                 com=self._page['com'],
-                # ids=self._page['user_id'],
                 semantic_url=self._page['semantic_url']
             ).save()
 
-        print('Complete..Updating...')
-        print('Update Complete...')
+        print('Updated')
+
         return self._page
 
 
-c = GetChan()
-c.get_catalog_page('pol', 9)
+# calling the 4chandata called class
+
+c = getting_chan_data()
+# invoking the method
+c.get_catalog_page('pol', 7)
