7119affebdd99e440f90ceff510d08e7cb9853dd
Bhuvan Chadha <bchadha1@binghamton.edu>
2020-11-27 20:24:05 -0500

    Data Analysis

--- DataAnalysis.py
+++ DataAnalysis.py
@@ -12,9 +12,37 @@ from bs4 import BeautifulSoup
 from nltk import word_tokenize, sent_tokenize
 from nltk.corpus import stopwords
 from nltk.stem import LancasterStemmer, WordNetLemmatizer
+import seaborn as sns
 
-#importing the dataset
-pol_comments = pd.read_csv("chan_final_data.csv")
+# importing the dataset
+pol_comments = pd.read_csv("4chan_final_data.csv")
 pol_comments.head(10)
+# seeing the available columns in the dataset
+pol_comments.columns
+# checking the shape of dataset
+pol_comments.shape
+# assigning the dataset to dataframe
+df = pd.DataFrame(pol_comments)
+df.shape
+# converting the comments into text
+df['com'] = df['com'].astype(str)
+# checking for one row
+df['com'][2]
+# to lowercase everything
+df['com'] = df['com'].apply(lambda x: " ".join(x.lower() for x in x.split()))
+df['com']
+# removing the punctuation
+df['com'] = df['com'].str.replace('[^\w\s]', '')
+df['com']
 
-print(pol_comments)
+
+def senti(x):
+    return TextBlob(x).sentiment
+
+
+df['senti_score'] = df['com'].apply(senti)
+mw = df['senti_score']
+mw
+
+sns.distplot([mw])
+#[mw].mean()
